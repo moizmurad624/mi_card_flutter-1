5256c237c802dffda08df122c9212f97f201893a
moizmurad624 <moiz.murad@tekrevol.com>
2024-09-03 00:04:58 +0530

    Mi_Card_Flutter Project
    None

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+  runApp(HotReload()
   );
 }
+
+
+class HotReload extends StatelessWidget {
+  const HotReload({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.amberAccent,
+                backgroundImage: AssetImage('Images/profile.svg'),
+              ),
+              Text('Moiz Murad Sheik',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              Text('Moiz Murad Sheik',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+
+
+            ],
+          )
+        )
+      ),
+    );
+  }
+}
+
+
